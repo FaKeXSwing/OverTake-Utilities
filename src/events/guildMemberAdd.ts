4122105597dd765035b05c8aecc00c5c219285e9
FaKeXSwing <151402158+FaKeXSwing@users.noreply.github.com>
2025-12-17 23:00:36 +0100

    Fixed welcome ping

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -7,7 +7,7 @@ export async function execute(client: Client, guildMember: GuildMember) {
 
     const welcomeEmbed = new EmbedBuilder()
         .setTitle("Welcome to OverTake Esports!")
-        .setDescription("Welcome to **OverTake Esports**, <@625777528560418836>!\n\nWe're excited to have you here. OverTake is built on competition, growth, and community. Whether you're here to grind, improve your skills, or just vibe with like-minded players, you're in the right place.")
+        .setDescription(`Welcome to **OverTake Esports**, <@${userId}>!\n\nWe're excited to have you here. OverTake is built on competition, growth, and community. Whether you're here to grind, improve your skills, or just vibe with like-minded players, you're in the right place.`)
         .setColor("#d768bb")
         .setImage("https://cdn.discordapp.com/attachments/1449408606821945529/1450948572811100210/Pinkbanner.jpg?ex=694464a9&is=69431329&hm=8d55d94f0fea138820ac7f93f8adae4ff3ecc4172e834aa61b7550755ec8468e&")
         .setAuthor({ name: "OverTake Esports", iconURL: "https://cdn.discordapp.com/attachments/1449408606821945529/1450948079477063720/PinkbannerPFP.jpg?ex=69446433&is=694312b3&hm=dc19c5caf99fff1d07d91b62e6f8e7e0429e71c432636739d7a6e9f75223d83b&" })
