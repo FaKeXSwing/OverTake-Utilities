6d7a56e30f7c4dfd8bdf5f78b44178420989de35
FaKeXSwing <151402158+FaKeXSwing@users.noreply.github.com>
2025-12-22 05:27:02 +0100

    Dashboard basics

---
+++ src/dashboard/routes/auth.ts
@@ -0,0 +1,56 @@
+import { Router } from "express";
+import axios from "axios";
+import config from "../../../config.json" with { type: "json" }
+const { clientIds } = config
+
+const router = Router();
+
+router.get("/discord", (_, res) => {
+    const params = new URLSearchParams({
+        client_id: clientIds.development,
+        redirect_url: process.env.REDIRECT_URI!,
+        response_type: "code",
+        scope: "identify guilds"
+    })
+
+    res.redirect(`https://discord.com/api/oauth2/authorize?${params.toString()}`)
+})
+
+router.get("/discord/callback", async (req, res) => {
+    const code = req.query.code as string;
+
+    const tokenRes = await axios.post(
+        "https://discord.com/api/oauth2/token",
+        new URLSearchParams({
+            client_id: clientIds.development,
+            client_secret: process.env.DEV_CLIENT_SECRET!,
+            grant_type: "authorization_code",
+            code,
+            redirect_url: process.env.REDIRECT_URI!
+        }),
+        {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        }
+    )
+
+    const accessToken = tokenRes.data.access_token;
+
+    const userRes = await axios.get("https://discord.com/api/users/@me", {
+        headers: { Authorization: `Bearer ${accessToken}` }
+    });
+
+    req.session.user = userRes.data;
+    req.session.accessToken = accessToken;
+
+    res.redirect("/dashboard")
+})
+
+router.get("/logout", (req, res) => {
+    // req.session.destroy(() => {
+    //     res.redirect("/");
+    // })
+    
+    res.redirect("/")
+})
+
+export default router
---
+++ src/dashboard/server.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import session from "express-session";
+import bodyParser from "body-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import authRoutes from "./routes/auth.js"
+// import dashboardRoutes from "./routes/dashboard.js"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET!,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1 * 60 * 1000
+        }
+    })
+)
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.use("/auth", authRoutes)
+// app.use("/dashboard", dashboardRoutes)
+
+app.get("/dashboard", (req, res) => {
+    if (!req.session.user)
+        return res.redirect("/auth/discord");
+
+    const user = req.session.user
+
+    res.sendFile(path.join(__dirname, "./public/html/dashboard.html"));
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/html/index.html"));
+})
+
+app.listen(3000, () => {
+    console.log("Dashboard running on http://localhost:3000")
+})
